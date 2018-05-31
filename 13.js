a30cb110cf2818a1f7fcf28137b4b1bdd76e7144
huan07 <845265003@qq.com>
2018-06-01 00:31:24 +0800

    .

--- 13.js
+++ 13.js
@@ -3,38 +3,45 @@
  */
 
 // Reflect.get 第一个参数不是对象，会报错的
-var myObject = {
-    foo: 11,
-    bar: 22,
-    get baz() {
-        return this.foo + this.bar;
-    }
-};
-console.log(Reflect.get(myObject, 'foo'), Reflect.get(myObject, 'bar'), Reflect.get(myObject, 'baz'));
+{
+    var myObject = {
+        foo: 11,
+        bar: 22,
+        get baz(){
+            return this.foo + this.bar;
+        }
+    };
+    console.log(Reflect.get(myObject, 'foo'), Reflect.get(myObject, 'bar'));
+    console.log(Reflect.get(myObject, 'baz'));
 
-var myReceiverObject = {
-    foo: 44,
-    bar: 55,
-};
-console.log(Reflect.get(myObject, 'baz', myReceiverObject));
+    var myReceiverObject = {
+        foo: 4,
+        bar: 5,
+    };
+    console.log(myObject, 'baz', myReceiverObject);
+}
+
 
 // Reflect.set 第一个参数不是对象，会报错的
-var myObject = {
-    foo: 66,
-    set bar(value) {
-        return this.foo = value;
-    },
-};
-console.log(myObject.foo);
+{
+    var myObject = {
+        foo: 66,
+        set bar(value){
+            return this.foo = value;
+        },
+    };
+    console.log(myObject.foo);
 
-Reflect.set(myObject, 'foo', 77);
-console.log(myObject.foo);
+    Reflect.set(myObject, 'foo', 77);
+    console.log(myObject.foo);
 
-var myReceiverObject = {
-    foo: 0,
-};
-Reflect.set(myObject, 'foo', 100, myReceiverObject);
-console.log(myObject.foo, myReceiverObject.foo);
+    var myReceiverObject = {
+        foo: 0,
+    };
+    Reflect.set(myObject, 'foo', 100, myReceiverObject);
+    console.log(myObject.foo, myReceiverObject.foo);
+}
+
 
 // Proxy 对象和 Reflect 对象联合使用，
 // 前者拦截赋值操作，后者完成赋值的默认行为
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-    entry: ['babel-polyfill', './12.js'],
+    entry: ['babel-polyfill', './13.js'],
 
     output: {
         filename: 'bundle.js',
