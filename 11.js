09c9c4823ad0ea79ad5e1a47d0431de204a4f7e6
huan07 <845265003@qq.com>
2018-07-22 13:04:25 +0800

    .

--- 11.js
+++ 11.js
@@ -2,12 +2,14 @@
  * Created by yangHuan on 17/9/12.
  */
 
-// 构造函数，Set数据结构  成员的值是唯一的(可以用来数组去重)
-// 1. 键名 键值是同一个值
+// Set数据结构  类似于数组，但是成员的值都是唯一的(可以用来数组去重)
+// Set 函数可以接受一个数组（或者具有 iterable 接口的其他数据结构）作为参数
 const defineSet = new Set();
 const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
+console.log(Object.prototype.toString.call(defineSet2));
 
-// NaN等于自身，两个对象总是不想等的 !!!!!!!!!!!!
+// Set内部判断两个值是否不同，类似于精确相等运算符（===），主要的区别是NaN等于自身
+// NaN等于自身，两个对象总是不想等的
 {
     const set = new Set();
     const a = NaN;
@@ -23,16 +25,18 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
     set.add({});
     console.log(set.size);
 }
+
+// Set 实例的方法: add, delete, has, clear
 {
     const set = new Set();
     set.add(1).add(2).add(2).add(3);
-    console.log('add size example => ', set.size, set);
+    console.log('add example => ', set.size, set);
 
     set.delete(1);
-    console.log('delete size example => ', set.has(1), set.has(2));
+    console.log('delete example => ', set.has(1), set.has(2));
 
     set.clear();
-    console.log('clear size example => ', set.size);
+    console.log('clear example => ', set.size);
 }
 
 // 数组去重：数组作为参数转换为Set数据结构，Set再被转换为数组
@@ -45,13 +49,15 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
     const array = [1, 2, '2', 2, 3];
     const items = new Set(array);
     const xx = [...items];
+
     const yy = dedupe(array);
-    console.log(Object.prototype.toString.call(items));
-    console.log('去重数组的方式之一 => ', xx, Object.prototype.toString.call(xx));
-    console.log('去重数组的方式之二 =>', yy, Object.prototype.toString.call(yy));
+    console.log('去重数组的方式之一 => ', xx);
+    console.log('去重数组的方式之二 =>', yy);
 }
 
-// keys(), values(), entries(),forEach()
+// Set 实例的方法: keys(), values(), entries(), 返回的都是遍历器对象
+// forEach();
+// Set数据结构 默认遍历器生成函数就是它的values方法
 {
     const set = new Set(['red', 'green', 'blue']);
     for (let item of set.keys()) {
@@ -60,11 +66,14 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
     for (let item of set.values()) { // 可以省略values() to add to add
         console.log(item);
     }
-    for (let item of set) {
+
+    for (let item of set.entries()) {
         console.log(item);
     }
 
-    for (let item of set.entries()) {
+    console.log(Set.prototype[Symbol.iterator] === Set.prototype.values); // why not true
+    console.log(set[Symbol.iterator] === set.entries); // why not true
+    for (let item of set) {
         console.log(item);
     }
 
@@ -76,7 +85,7 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
 // map filter 间接用于Set
 {
     let set = new Set([1, 2, 3]);
-    set = new Set([...set].map(x => x + 100));
+    set = new Set([...set].map(x => x + 1000));
     console.log(set);
 }
 {
@@ -84,8 +93,10 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
     set = new Set([...set].filter(x => x % 2 === 1));
     console.log(set);
 }
+
+// 数组并集/交集/差集
 {
-    console.log('Set => union intersect difference==数组并集==数组交集==数组差集===');
+    console.log('Set => union intersect difference');
     const arr1 = [1, 2, 3];
     const arr2 = [2, 3, 4];
     const set1 = new Set(arr1);
@@ -100,7 +111,9 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
 }
 
 
-// 2.WeakSet 它的对象都是弱引用；不可遍历
+// 2.WeakSet
+// a.WeakSet 的成员只能是对象（任何具有 Iterable 接口的对象，都可以作为 WeakSet 的参数）
+// b.WeakSet 中的对象都是弱引用 to add
 {
     const ws = new WeakSet();
     // ws.add(1); error
@@ -111,7 +124,8 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
     const ws = new WeakSet();
 
     const a = [[1, 2], [3, 4]];
-    const ws2 = new WeakSet(a); // 参数是数组，或类数组对象；成员只能是对象
+    const ws2 = new WeakSet(a);
+    console.log('ws, ws2 => ', ws, ws2);
 }
 
 
@@ -125,15 +139,16 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
 
 // set get size has delete clear
 // 引用类型值，按内存地址 对同一个对象的引用，Map结构才将其视为同一个key
-// 简单类型的值，严格相等，Map结构才将其视为同一个key
-// keys values entries forEach
+// 简单类型值，严格相等，Map结构才将其视为同一个key
+// 实例方法：keys values entries forEach
 {
     const defineMap = new Map();
     const o = { p: 'hello baidu' };
-    defineMap.set(o, 'baidu');  // 返回整个 Map 结构
+
+    defineMap.set(o, 'baidu');  // 返回的是当前的Map对象，可以采用链式写法
     console.log('defineMap.get(o)=>', defineMap.get(o), defineMap.size);
 
-    console.log(defineMap.has(o), defineMap);
+    console.log(defineMap.has(o));
     defineMap.delete(o);
     console.log(defineMap.has(o));
 }
@@ -142,7 +157,7 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
         ['name', '张三'],
         ['title', 'author']
     ]); // 每个成员是一个双元素的数组
-    console.log(defineMap2.size, defineMap2.has('name'), defineMap2.get('name'));
+    console.log(defineMap2.get('name'), defineMap2.size, defineMap2.has('name'),);
 
     for (let key of defineMap2.keys()) {
         console.log('1=>', key);
@@ -152,26 +167,22 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
         console.log('2=>', value);
     }
 
-    for (let item of defineMap2.entries()) {
-        console.log('3=>', item[0], item[1], item);
-    }
-
     for (let [key, value] of defineMap2.entries()) {
         console.log('4=>', key, value);
     }
+    console.log(defineMap2[Symbol.iterator] === defineMap2.entries);
 
     for (let [key, value] of defineMap2) {
         console.log('44=>', key, value);
     }
-
-    console.log(defineMap2.entries);
 
     console.log(defineMap2, defineMap2.keys(), defineMap2.values(), defineMap2.entries());
     console.log('Map结构转换为数组  扩展运算符[... Map]');
     console.log([...defineMap2], [...defineMap2.keys()], [...defineMap2.values()], [...defineMap2.entries()]);
 }
 
-// Map的遍历和过滤 map, filter
+// Map的间接map,filter
+// Map的forEach
 {
     const map0 = new Map()
         .set(1, 'a')
@@ -181,9 +192,13 @@ const defineSet2 = new Set([1, 2, 3, 4, 4, 3, 2, 1]);
 
     const array1 = array0.filter(([k, v]) => k <= 2);
 
-    const array2 = array0.map(([k, v]) => [k + 100, v + 'z']);
+    const array2 = array0.map(([k, v]) => [k + '_key', v + '_value']);
 
-    console.log('array1 => ', array1, new Map(array1), array2, new Map(array2))
+    console.log('map0 => ', map0, array0);
+    console.log('map1 => ', new Map(array1), array1);
+    console.log('map2 => ', new Map(array2), array2);
+
+    new Map(array2).forEach((value, key, map) => console.log(key, value));
 }
 // to add example
 
